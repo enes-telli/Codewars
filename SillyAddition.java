cb5c1612c673a562efb5b03662023460cff8d88e
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-08 18:32:04 +0300

    Update SillyAddition.java

--- SillyAddition.java
+++ SillyAddition.java
@@ -1,37 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg16.pkg18.pkg214.silly.addition;
 
-/**
- *
- * @author Enes Telli
- */
-public class SillyAddition {
-
-    public static int add(int num1,int num2){
+public static int add(int num1,int num2){
         
-        String max = Integer.toString(num1 >= num2 ? num1 : num2);
-        String min = Integer.toString(num1 >= num2 ? num2: num1);
-        String result = "";
-        String temp = "";
+    String max = Integer.toString(num1 >= num2 ? num1 : num2);
+    String min = Integer.toString(num1 >= num2 ? num2: num1);
+    String result = "";
+    String temp = "";
         
-        for(int i = 0; i < max.length() - min.length(); i++)
-        {
-            temp += "0";
-        }
-        min = temp.concat(min);
-        for(int i = 0; i < max.length(); i++)
-        {
-            result += Integer.parseInt("" + min.charAt(i)) + Integer.parseInt("" + max.charAt(i));
-        }
-        return Integer.parseInt(result);
+    for(int i = 0; i < max.length() - min.length(); i++)
+    {
+        temp += "0";
     }
-    
-    public static void main(String[] args) {
-        System.out.println(SillyAddition.add(592,1376));
+    min = temp.concat(min);
+    for(int i = 0; i < max.length(); i++)
+    {
+        result += Integer.parseInt("" + min.charAt(i)) + Integer.parseInt("" + max.charAt(i));
     }
-    
+    return Integer.parseInt(result);
 }
+    
