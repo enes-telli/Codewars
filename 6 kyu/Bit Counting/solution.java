3b17fe4a415b72efd485ce0f9f94004ce21ccd1f
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-11 22:40:57 +0300

    Create solution.java

---
+++ 6 kyu/Bit Counting/solution.java
@@ -0,0 +1,12 @@
+public class BitCounting {
+    public static int countBits(int n){
+		    int result = 0;
+        while(n > 0)
+        {
+            if(n % 2 == 1)
+                result++;
+            n >>= 1;
+        }
+        return result;
+	  }
+}
