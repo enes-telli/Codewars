4f2877292b18d773dad97935c6a989dcda9ef137
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-11 16:14:24 +0300

    Update solution.java

--- 8 kyu/Is n divisible by x and y?/solution.java
+++ 8 kyu/Is n divisible by x and y?/solution.java
@@ -1,5 +1,5 @@
 public class DivisibleNb {
-	  public static boolean isDivisible(long n, long x, long y) {
-	  	  return n % x == 0 && n % y == 0 ? true : false;
-	  }
+    public static boolean isDivisible(long n, long x, long y) {
+        return n % x == 0 && n % y == 0 ? true : false;
+    }
 }
