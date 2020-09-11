bbb9aac52a0ab237cdaa33143d716f539be0fa89
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-11 22:43:56 +0300

    Update solution2.java

--- 6 kyu/Bit Counting/solution2.java
+++ 6 kyu/Bit Counting/solution2.java
@@ -1,5 +1,5 @@
 public class BitCounting {
-    public static int countBits(int n){
-		    return Integer.bitCount(n);
-	  }
+    public static int countBits(int n) {
+        return Integer.bitCount(n);
+    }
 }
