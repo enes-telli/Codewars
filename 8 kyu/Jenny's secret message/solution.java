338a84ecc9cb5d027c629460e7dbe1121549325c
Enes Telli <55920002+enes-telli@users.noreply.github.com>
2020-09-15 19:36:50 +0300

    Create solution.java

---
+++ 8 kyu/Jenny's secret message/solution.java
@@ -0,0 +1,8 @@
+public class Greeter {
+    public static String greet(String name) {
+        if(name.equals("Johnny")) {
+            return "Hello, my love!";
+        }
+        return "Hello, " + name + "!";
+    }
+}
