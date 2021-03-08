90e08a44bfe1c616c6c3ea5c1fe8f3e12be81f62
Enes Telli <enes1903telli@gmail.com>
2021-03-08 15:59:23 +0300

    New Solutions

---
+++ 7 kyu/Over The Road/solution.java
@@ -0,0 +1,5 @@
+class CodeWars {
+    public static long overTheRoad(long address, long houses) {
+        return 2 * houses + 1 - address;
+    }
+}
---
+++ 8 kyu/The Feast of Many Beasts/solution.java
@@ -0,0 +1,5 @@
+public class Kata {
+    public static boolean feast(String beast, String dish) {
+        return (beast.charAt(0) != dish.charAt(0) || beast.charAt(beast.length() - 1) != dish.charAt(dish.length() - 1)) ? false : true;
+    }
+}
